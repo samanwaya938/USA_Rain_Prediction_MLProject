790ec768b0a88f27b14c2caba7d25088eef7a03f
samanwaya938 <samanwayaghosh938@gmail.com>
2025-03-14 02:26:56 +0530

    File structure added

---
+++ template.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+import logging
+
+project_name = "USA_Rain_Prediction_MLProject"
+list_of_files = [
+  ".github/workflows/.gitkeep",
+  "Data/"
+  f"src/{project_name}/__init__.py",
+  f"src/{project_name}/components/__init__.py",
+  f"src/{project_name}/utils/__init__.py",
+  f"src/{project_name}/logger/__init__.py",
+  f"src/{project_name}/exception/__init__.py",
+  f"src/{project_name}/config/__init__.py",
+  f"src/{project_name}/pipeline/__init__.py",
+  "experiment/test.ipynb",
+  "requirements.txt",
+  "params.yaml"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
